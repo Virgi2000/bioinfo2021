278767158efa891b34b868fa9a0e7c93bde8b1b8
Virgi2000 <87233814+Virgi2000@users.noreply.github.com>
2021-08-04 11:58:16 -0500

    Virginia ha cambiado

--- dalziel.py
+++ dalziel.py
@@ -1,54 +1,6 @@
 # FUNCIÓN 1. de creación de una población con alelos al azar
 
-import dalziel as dz
-
-
-
-with open ('abc.txt') as datos:
-    suma= 0
-    numero_personas = 0
-    i = 0
-    for line in datos:
-        if i > 0:
-            line = line.split(',')
-            edad = int(line[1])
-            nota = float(line[2])
-            if edad >= 18:
-                suma += nota
-                numero_personas += 1
-    i += 1
-    print(suma/numero_personas)
-
-cities = "BalTiMOre"
-cities = ["BalTiMOre", "COLUMBUS", "miami") # de ser necesario debe primero uniformizar los nombres ingresados
-
-a = dz.avgcity(cities)
-
-years = 1920
-years = [1920, 1930, 1948]
-
-b = dz.avgyear(cities, year)
-
-weeks = 12
-weeks = [1, 6]
-weeks = range(12:24)
-c = dz.avgbiweek(cities, biweeks)
-
-import scipy # for random numbers
-def build_population(N, p):
-    """The population consists of N individuals.Each individual has two chromosomes, containing
-    allele "A" or "a", with probability p or 1-p, respectively.The population is a list of tuples.
-    """
-    population = []
-    for i in range(N):
-        allele1 = "A"
-        if scipy.random.rand() > p:
-            allele1 = "a"
-        allele2 = "A"
-        if scipy.random.rand() > p:
-            allele2 = "a"
-        population.append((allele1, allele2))
-    return population
+asdfghjk
 
 # FUNCIÓN 2. Cáculo de la infecciones promedio por año, la desviación estándar y número de registros
 def compute_frequencies(population):
